4fd39b5e525c2f592e0f8ca293a022584b28470b
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2024-01-28 10:00:00 +0530

    add: header only Socket

---
+++ Socket.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <arpa/inet.h>
+#include <functional>
+#include <sys/socket.h>
+#include <system_error>
+#include <unistd.h>
+
+
+struct Network {
+    sockaddr_in address{};
+    socklen_t length;
+
+    Network(const char* ipv4_address, const unsigned short int port) {
+        this->address.sin_family = AF_INET;
+        this->address.sin_port = htons(port);
+        this->address.sin_addr.s_addr = inet_addr(ipv4_address);
+        this->length = sizeof(address);
+    }
+};
+
+
+class Socket {
+private:
+    sockaddr_in address{};
+    socklen_t length;
+
+public:
+    int udpsocket;
+
+    Socket(const Network& network) {
+        this->address.sin_family = network.address.sin_family;
+        this->address.sin_port = network.address.sin_port;
+        this->address.sin_addr.s_addr = network.address.sin_addr.s_addr;
+        this->length = network.length;
+
+        this->udpsocket = socket(AF_INET, SOCK_DGRAM, 0);
+        
+        if (this->udpsocket == -1)
+            throw std::runtime_error("failed to create socket");
+        else
+            bind(this->udpsocket, (sockaddr*)&this->address, this->length);
+    }
+
+    ~Socket() {
+        if (this->udpsocket > 0) close(this->udpsocket);
+    }
+
+
+    ssize_t Send(const unsigned char* data, const unsigned int size, const Network& send_to) {
+        ssize_t bytes_sent = sendto(
+            this->udpsocket,
+            data, size,
+            MSG_CONFIRM | MSG_NOSIGNAL,
+            (struct sockaddr*)&send_to.address, send_to.length
+        );
+
+        return bytes_sent;
+    }
+
+
+    ssize_t Receive(std::function<void(unsigned char*, int)> callback, const unsigned int size) {
+        unsigned char buffer[size] = {0};
+        
+        ssize_t bytes_read = recvfrom(
+            this->udpsocket,
+            buffer, size,
+            0,
+            (struct sockaddr*)&this->address, &this->length
+        );
+
+        if (bytes_read > 0) callback(buffer, bytes_read);
+
+        return bytes_read;
+    }
+};
